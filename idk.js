96226053f226f7d0ca2cc75d384c57f6df2c3020
Lalit Jharbade <lalit.sonu06@gmail.com>
2016-06-19 20:27:12 +0530

    css and javascript files for idk

---
+++ idk.js
@@ -0,0 +1,7 @@
+var naaame;
+function naame(){naaame = prompt("What's your name?");return naaame;}
+function hello(){alert("what the hell are you doing?");}
+function rsize(){alert("What's wrong with you "+naaame);}
+function message(msg){ document.getElementById('op').innerHTML = msg + "What's wrong with you ";}
+function displayDate(){ document.getElementById('op').innerHTML = Date();}
+
